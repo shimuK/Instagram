124856313315ba708ff5331ba2ae1d0ffafbdac7
Kei Shimura <shimura_kei@r.recruit.co.jp>
2021-07-29 20:22:53 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 志村　圭 on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
