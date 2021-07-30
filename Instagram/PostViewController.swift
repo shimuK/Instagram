7bb8d6940da7161829607a9f08bf56883335cb0f
Kei Shimura <shimura_kei@r.recruit.co.jp>
2021-07-30 11:53:07 +0900

    FireBase設定まで

---
+++ Instagram/PostViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by 志村　圭 on 2021/07/29.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Instagram/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 志村　圭 on 2021/07/29.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
